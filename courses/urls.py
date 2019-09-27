c8ec7d46f429a9a1f630365a9803b4cf4928392b
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-27 23:51:26 +0300

    working with templates to get courses and lessons, in views

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CourseListView, CourseDetailView
+from .views import CourseListView, CourseDetailView, LessonDetailView
 
 
 
@@ -8,6 +8,7 @@ from .views import CourseListView, CourseDetailView
 urlpatterns = [
     path('', CourseListView.as_view(), name='list'),
     path('<slug>', CourseDetailView.as_view(), name='detail'),
+    path('<course_slug>/<lesson_slug>', LessonDetailView.as_view(), name='lesson-detail')
 ]
 
 
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Lesson, Course
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 
 
 class CourseListView(ListView):
@@ -12,3 +12,26 @@ class CourseListView(ListView):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course_detail.html'
+
+
+class LessonDetailView(View):
+
+    def get(self, request, course_slug, lesson_slug, *args, **kwargs):
+
+        """Grab course for this lesson"""
+        course_qs = Course.objects.filter(slug=course_slug)
+        if course_qs.exists():
+            course = course_qs.first()
+
+        lesson_qs = course.lessons.filter(slug=lesson_slug)
+        if lesson_qs.exists():
+            lesson = lesson_qs.first()
+
+
+        context = {
+            'object': lesson
+        }
+
+        return render(request, 'courses/lesson_detail.html', context)
+
+
